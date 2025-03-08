a5e7340851808cead42b8c81158db57f2693b664
kohinoor27 <tiwarikohinoor2004@gmail.com>
2025-03-08 19:11:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public abstract class Logger 
+    {
+        public abstract void Log(string message);
+
+    }
+}
+public class DatabaseLogger : Logger
+{
+    public override void Log(string message)
+    {
+        Console.WriteLine("Log written to Database: {message}");
+
+    }
+}
+public class FileLogger : Logger
+{
+    public override void Log(string message)
+    {
+        Console.WriteLine("Log written to File : {message}");
+    }
+
+}
+public class CloudLogger : Logger
+{
+    public override void Log(string message)
+    {
+        Console.WriteLine("Log written to Cloud : {message}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+      
+        Logger dbLogger = new DatabaseLogger();
+        Logger fileLogger = new FileLogger();
+        Logger cloudLogger = new CloudLogger();
+
+       
+        dbLogger.Log("This is a database log");
+        fileLogger.Log("This is a file log");
+        cloudLogger.Log("This is a cloud log");
+    }
+}
