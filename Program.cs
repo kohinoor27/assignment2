afd51623086fa0d24992e2e0dd58f93f163bb82c
kohinoor27 <tiwarikohinoor2004@gmail.com>
2025-03-13 20:37:29 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,185 @@
-﻿using ConsoleApp4;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp6
 {
-    public abstract class Logger 
+    public class Program
     {
-        public abstract void Log(string message);
+        public static void Main(string[] args)
+        {
+            Customer customer = new GoldCustomerRegister
+            {
+                CustomerId = 2,
+                FirstName = "Kohinoor",
+                LastName = "Tiwari",
+                Email = "tiwarikohinoor@gmail.com"
+            };
+            customer.Register();
+
+            ICustomerGetDiscount newDiscount = new GoldCustomerDiscount();
+            newDiscount.CustomerDiscountPercentage();
+
+            IProcessOrder process = new ProcessOrderCLass(newDiscount);
+            process.ProcessOrder();
+        }
+    }
+  
+    
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public virtual void Register()
+        {
+            Console.WriteLine("Customer is Registered!!");
+        }
+    }
+
+    public class PlatinumCustomerRegister : Customer
+    {
+        public override void Register()
+        {
+            Console.WriteLine($"{FirstName} is registered as a platinum customer!!");
+        }
+    }
+
+    public class GoldCustomerRegister : Customer
+    {
+        public override void Register()
+        {
+            Console.WriteLine($"{FirstName} is registered as a gold customer!!");
+        }
 
     }
-}
-public class DatabaseLogger : Logger
-{
-    public override void Log(string message)
-    {
-        Console.WriteLine("Log written to Database: {message}");
 
-    }
-}
-public class FileLogger : Logger
-{
-    public override void Log(string message)
+    public class SilverCustomerRegister : Customer
     {
-        Console.WriteLine("Log written to File : {message}");
+        public override void Register()
+        {
+            Console.WriteLine($"{FirstName} is registered as a silver customer!!");
+        }
     }
 
-}
-public class CloudLogger : Logger
-{
-    public override void Log(string message)
+    public class BronzeCustomerRegister : Customer
     {
-        Console.WriteLine("Log written to Cloud : {message}");
+        public override void Register()
+        {
+            Console.WriteLine($"{FirstName} is registered as a bronze customer!!");
+        }
     }
-}
-class Program
-{
-    static void Main()
-    {
-      
-        Logger dbLogger = new DatabaseLogger();
-        Logger fileLogger = new FileLogger();
-        Logger cloudLogger = new CloudLogger();
 
-       
-        dbLogger.Log("This is a database log");
-        fileLogger.Log("This is a file log");
-        cloudLogger.Log("This is a cloud log");
+
+
+    public interface ISaveCustomer
+    {
+        bool SaveCustomer();
+    }
+
+    public class SavedCustomer : ISaveCustomer
+    {
+        private Customer _customer;
+
+        public SavedCustomer(Customer customer)
+        {
+            _customer = customer;
+        }
+
+        public bool SaveCustomer()
+        {
+            Console.WriteLine($"{_customer.FirstName} is saved to th db.");
+            return true;
+        }
+
+    }
+
+    public interface ICustomerGetDiscount
+    {
+        int CustomerDiscountPercentage();
+    }
+
+    public class PlatinumCustomerDiscount : ICustomerGetDiscount
+    {
+        public int CustomerDiscountPercentage()
+        {
+            return 20;
+        }
+    }
+
+    public class GoldCustomerDiscount : ICustomerGetDiscount
+    {
+        public int CustomerDiscountPercentage()
+        {
+            return 25;
+        }
+    }
+
+
+    public class SilverCustomerDiscount : ICustomerGetDiscount
+    {
+        public int CustomerDiscountPercentage()
+        {
+            return 15;
+        }
+    }
+
+    public class BronzeCustomerDiscount : ICustomerGetDiscount
+    {
+        public int CustomerDiscountPercentage()
+        {
+            return 25;
+        }
+    }
+
+
+    public interface IProcessOrder
+    {
+        void ProcessOrder();
+    }
+
+    public class ProcessOrderCLass : IProcessOrder
+    {
+        private ICustomerGetDiscount _getDiscount;
+
+        public ProcessOrderCLass(ICustomerGetDiscount  getDiscount)
+        {
+            _getDiscount = getDiscount;
+        }
+
+        public void ProcessOrder()
+        {
+            var discount = _getDiscount.CustomerDiscountPercentage();
+            Console.WriteLine($"Customer has got discount of {discount}");
+            Console.WriteLine("Order is Placed");
+        }
+
+
+    }
+
+    public class Leads
+    {
+        public string name;
+        public string email;
+
+        public void GetLeadDetails()
+        {
+            Console.WriteLine($"chating with {name} ");
+        }
     }
 }
+
+
+    
+
+    
+    
+         
+    
+ 
+
+
